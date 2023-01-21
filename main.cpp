b872d4c727af4ba2894f7d8d5dd3c62a19919e0b
CalebHaleDev <107201448+CalebHaleDev@users.noreply.github.com>
2023-01-21 12:26:31 -0700

    add a help section and reset

--- main.cpp
+++ main.cpp
@@ -15,30 +15,54 @@ void printLargeBoard();
 void printScreen();
 void checkWins(char board[3][3]);
 void changeTurn();
+void reset();
 static char board[3][3] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
-//static char largeBoard[9][3][3] = {{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}};
+static char largeBoard[9][3][3] = {{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}},{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}};
+static char pointBoard[3][3] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
 char winner = ' ';
-char turn = 'X';
+static char turn = 'X';
 int gamemode;
-int selection;
+int selection = 0;
 char redo;
 
-//system("pause");    //reminder that I could use this later if I want to
+
 
 int main() {
-    cout << "type 2 for a standard game and 3 for 3d tic tac toe: ";
+    reset();
+
+    cout << "type 2 for a standard game, 3 for 3d tic tac toe, or 0 for help: ";
     cin >> gamemode;
-    if (gamemode == 2){
+    if (gamemode == 0){
+        cout << endl << endl;
+        cout << "This is the standard board, spots labelled with their input numbers:"<< endl;
+        cout << endl;
+        cout << 1 << " | " << 2 << " | " << 3 << endl;
+        cout << 4 << " | " << 5 << " | " << 6 << endl;
+        cout << 7 << " | " << 8 << " | " << 9 << endl;
+        cout << endl;
+        cout << "To play, type the number of the square you want to play in." << endl;
+        cout << "You can quit at any time by entering 0" << endl;
+        cout << "In the 3d version the spot where each player goes determines which board the next player plays in." << endl;
+        cout << "So... you're playing nine games at the same time and you win by a point system." << endl;
+        cout << "Each game you win is a point and each three-in-a-row win is a point." << endl << endl;
+        cout << "When you're ready to head back to the menu, press a key." << endl;
+        system("pause");
+        main();
+        return 0;
+    }
+    else if (gamemode == 2){
         while(winner==' '){
             cout << endl;
             printScreen();
             cout << endl;
-            cout << "type a digit 1-9 to select a square (i.e. 2 for the top middle): ";
+            cout << "type a digit 1-9 to select a square: ";
+            if (selection==0)
+                cout << " (i.e. 2 for the top middle)";
             cin >> selection;
             if(selection==0)
                 return 0;
             board[((selection-1)%3)][((int)((selection-1)/3))] = turn;       //place X or O
-            cout << "You have selected " << (selection%3) << " and " << ((int)((selection-1)/3)) << endl;  //for troubleshooting
+            //cout << "You have selected " << (selection%3) << " and " << ((int)((selection-1)/3)) << endl;  //for troubleshooting
         changeTurn();
         checkWins(board);
     }
@@ -58,6 +82,18 @@ int main() {
     } 
     return 0;
 }
+
+void reset(){
+    winner = ' ';
+    turn = 'X';
+    for(int i=1;i<10;i++){
+        board[((i-1)%3)][((int)((i-1)/3))] = ' ';
+        pointBoard[((i-1)%3)][((int)((i-1)/3))] = ' ';
+        for(int j=1;j<10;j++)
+            largeBoard[j][((i-1)%3)][((int)((i-1)/3))] = ' ';
+    }
+    return;
+}; 
 
 
 void printScreen(){
